ed5195dee4dab82d0870b088e6dc452312fc9803
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-09-11 21:29:03 -0300

    :package: chore: Added shadcn ui tailwindcss and radix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Button } from "./components/ui/button"
 export function App() {
 
-  return (
-    <h1>Hello world</h1>
-      )
+    return (
+      <div>
+        <div className="px-6 py-6 flex items-center justify-between border-b">
+          <h1>upload.ai</h1>
+          <div>
+            <span>Desenvolvido com 💙</span>
+            <Button>GitHub</Button>
+          </div>
+        </div>
+      </div>
+    )
 }
 
